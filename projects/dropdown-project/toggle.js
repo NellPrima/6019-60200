d465d630a8fd29e3affe0bfda18fde76d4174546
Chanell Cousin <nelltheowl@gmail.com>
2024-10-09 00:47:59 -0400

    Change back later

--- projects/dropdown-project/toggle.js
+++ projects/dropdown-project/toggle.js
@@ -1,17 +1,1 @@
-const menu = document.querySelector(".fa-solid fa-bars");
-const dropDown = document.querySelector("#dropdownMenu");
-
-function openMenu() {
-    dropDown.classList.toggle('show');
-}
-
-menu.addEventListener('click', openMenu);
-
-// function openMenu() {
-//     if (dropDown.style.display === "none") {
-//         dropDown.classList.toggle('show');
-//     } else {
-//         dropDown.style.display = "block";
-//     }
-// }
-
+const navBar = document.querySelector("#navbar");
