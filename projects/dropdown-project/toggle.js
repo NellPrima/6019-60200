6681165cd909cd1aac0d720e60f2ea1f6b4ec14b
Chanell Cousin <nelltheowl@gmail.com>
2024-11-13 23:37:55 -0500

    sync

--- projects/dropdown-project/toggle.js
+++ projects/dropdown-project/toggle.js
@@ -1,1 +1,31 @@
-const navBar = document.querySelector("#navbar");
+const aMenu = document.getElementById("aMenu");
+const menuBttn = document.getElementById("menu_bttn");
+const button = document.getElementById("button");
+
+
+function openMenu() {
+    const computedStyle = window.getComputedStyle(aMenu);
+    if ( computedStyle.visibility === "hidden") {
+        aMenu.style.visibility = "visible";
+    } 
+
+}
+
+ function closeMenu() {
+    const computedStyle = window.getComputedStyle(aMenu);
+    if ( computedStyle.visibility === "visible") {
+        aMenu.style.visibility = "hidden";
+    } 
+}
+
+ button.addEventListener("click", function () {
+     const computedStyle = window.getComputedStyle(aMenu);
+     if (computedStyle.visibility === "hidden") {
+         openMenu();
+     }
+
+     else if (computedStyle.visibility === "visible") {
+         closeMenu();
+     }
+
+ });
